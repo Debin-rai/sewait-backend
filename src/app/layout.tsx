5f647077799def408d3f8d98c17d48de71a3bc6a
Debin-rai <rai.debin@gmail.com>
2026-02-16 21:53:30 +0545

    Self-host Material Symbols font for reliable icon display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ne" className="light" suppressHydrationWarning>
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block" />
       </head>
       <body
         className={`${inter.variable} ${mukta.variable} ${notoSansDevanagari.variable} antialiased bg-background-light text-slate-800 font-body`}
